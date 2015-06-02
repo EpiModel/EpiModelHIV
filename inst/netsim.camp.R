c6a8b26ec3cec2d583160d93ef4d73828a661e73
Samuel Jenness <sjenness@uw.edu>
2015-06-01 21:42:11 -0700

    Update netsim.mard file

--- inst/netsim.camp.R
+++ inst/netsim.camp.R
@@ -1,64 +1,30 @@
 
-#' @export
-netsim.camp <- function(x, param, init, control){
+x <- est
 
-  dat <- initialize.camp(x, param, init, control, s = 1)
+netsim.mard <- function(x, param, init, control){
+
+  dat <- initialize.mard(x, param, init, control, s = 1)
 
   for (at in max(2, control$start):control$nsteps) {
 
-    # Age
-    dat <- aging.camp(dat, at)
-
-    # Deaths
-    dat <- deaths.camp(dat, at)
-
-    # Births
-    dat <- births.camp(dat, at)
-
-    # Testing
-    dat <- test.camp(dat, at)
-
-    # Treatment
-    dat <- tx.camp(dat, at)
-
-    # Progress
-    dat <- progress.camp(dat, at)
-
-    # Viral load
-    dat <- update_vl.camp(dat, at)
-
-    # Change inst.ai.class
-    dat <- update_aiclass.camp(dat, at)
-
-    # Change role.class
-    dat <- update_roleclass.camp(dat, at)
-
-    # Update edges terms
-    dat <- edges_correct.camp(dat, at)
-
-    # Relational dynamics
-    dat <- resim_nets.camp(dat, at)
-
-    # Disclosure
-    dat <- disclose.camp(dat, at)
-
-    # Discordant AI: Main / Pers / Inst
-    dat <- acts.camp(dat, at)
-
-    # Condom use: Main / Pers / Inst
-    dat <- condoms.camp(dat, at)
-
-    # Position: Main / Pers / Inst
-    dat <- position.camp(dat, at)
-
-    # Transmission
-    dat <- trans.camp(dat, at)
-
-    # Summary statistics
-    dat <- getprev.camp(dat, at)
-
-    # Console output
-    verbose.camp(dat, type = "progress", s = 1, at)
+    dat <- aging.mard(dat, at)
+    dat <- deaths.mard(dat, at)
+    dat <- births.mard(dat, at)
+    dat <- test.mard(dat, at)
+    dat <- tx.mard(dat, at)
+    dat <- progress.mard(dat, at)
+    dat <- update_vl.mard(dat, at)
+    dat <- update_aiclass.mard(dat, at)
+    dat <- update_roleclass.mard(dat, at)
+    dat <- edges_correct.mard(dat, at)
+    dat <- simnet.mard(dat, at)
+    dat <- disclose.mard(dat, at)
+    dat <- acts.mard(dat, at)
+    dat <- condoms.mard(dat, at)
+    dat <- position.mard(dat, at)
+    dat <- trans.mard(dat, at)
+    dat <- prevalence.mard(dat, at)
+    verbose.mard(dat, type = "progress", s = 1, at)
 
   }
 
