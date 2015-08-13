19d8ab73fc74abac1c2d7452ba10a496cec49bb0
Samuel Jenness <sjenness@uw.edu>
2015-08-12 17:20:57 -0700

    Update run files

--- inst/netsim.mods.R
+++ inst/netsim.mods.R
@@ -1,32 +1,9 @@
 
-at <- 2
-dat <- initialize.mard(est, param, init, control, s = 1)
-for (at in max(2, control$start):control$nsteps) {
-  dat <- aging.mard(dat, at)
-  dat <- deaths.mard(dat, at)
-  dat <- births.mard(dat, at)
-  dat <- test.mard(dat, at)
-  dat <- tx.mard(dat, at)
-  dat <- prep.mard(dat, at)
-  dat <- progress.mard(dat, at)
-  dat <- update_vl.mard(dat, at)
-  dat <- update_aiclass.mard(dat, at)
-  dat <- update_roleclass.mard(dat, at)
-  dat <- edges_correct.mard(dat, at)
-  dat <- simnet.mard(dat, at)
-  dat <- disclose.mard(dat, at)
-  dat <- acts.mard(dat, at)
-  dat <- condoms.mard(dat, at)
-  dat <- position.mard(dat, at)
-  dat <- trans.mard(dat, at)
-  dat <- prevalence.mard(dat, at)
-  verbose.mard(dat, type = "progress", s = 1, at)
-}
-
-
-at <- 2
+at <- 1
 dat <- initialize.mard(est, param, init, control, s = 1)
 # dat <- reinit.mard(sim, param, init, control, s = 1)
+
+at <- at + 1
 dat <- aging.mard(dat, at)
 dat <- deaths.mard(dat, at)
 dat <- births.mard(dat, at)
@@ -42,8 +19,9 @@ dat <- simnet.mard(dat, at)
 dat <- disclose.mard(dat, at)
 dat <- acts.mard(dat, at)
 dat <- condoms.mard(dat, at)
+dat <- riskhist.mard(dat, at)
 dat <- position.mard(dat, at)
 dat <- trans.mard(dat, at)
 dat <- prevalence.mard(dat, at)
 verbose.mard(dat, type = "progress", s = 1, at)
-at <- at + 1
+
