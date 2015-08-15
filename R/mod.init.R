71aead5eb4b86d37baaaf6572c85ce1269e73526
Samuel Jenness <sjenness@uw.edu>
2015-08-14 17:20:48 -0700

    Add UAI group to init module

--- R/mod.init.R
+++ R/mod.init.R
@@ -85,6 +85,14 @@ initialize.mard <- function(x, param, init, control, s) {
   # Risk group
   dat$attr$riskg <- dat$nw[[3]] %v% "riskg"
 
+  # UAI group
+  p1 <- dat$param$cond.pers.always.prob
+  p2 <- dat$param$cond.inst.always.prob
+  rho <- dat$param$cond.always.prob.corr
+  uai.always <- rmvbin(num, c(p1, p2), bincorr = (1 - rho) * diag(2) + rho)
+  dat$attr$cond.always.casl <- uai.always[, 1]
+  dat$attr$cond.always.inst <- uai.always[, 2]
+
   # Arrival and departure
   dat$attr$arrival.time <- rep(1, num)
   dat$attr$depart.time <- rep(NA, num)
