291c0cbacbdbb9c8cc7f163e4411de9a6a3a193d
Samuel Jenness <sjenness@uw.edu>
2015-08-19 15:05:39 -0700

    Major overall of PrEP module

--- R/mod.prep.R
+++ R/mod.prep.R
@@ -1,5 +1,4 @@
 
-
 #' @title PrEP Module
 #'
 #' @description Module function for implementation and uptake of pre-exposure
@@ -11,71 +10,68 @@
 #'
 prep.mard <- function(dat, at) {
 
+  if (at < dat$param$prep.start) {
+    return(dat)
+  }
+
   ## Variables
   active <- dat$attr$active
   status <- dat$attr$status
   diag.status <- dat$attr$diag.status
+  lnt <- dat$attr$last.neg.test
+
   prepElig <- dat$attr$prepElig
-  prepEligTime <- dat$attr$prepEligTime
   prepStat <- dat$attr$prepStat
-  prepStartTime <- dat$attr$prepStartTime
   prepEver <- dat$attr$prepEver
   prepClass <- dat$attr$prepClass
 
-  prep.start <- dat$param$prep.start
   prep.elig.model <- dat$param$prep.elig.model
   prep.coverage <- dat$param$prep.coverage
   prep.cov.method <- dat$param$prep.cov.method
   prep.cov.rate <- dat$param$prep.cov.rate
   prep.class.prob <- dat$param$prep.class.prob
+  prep.risk.reassess <- dat$param$prep.risk.reassess
 
+# browser()
+  ## Eligibility ---------------------------------------------------------------
 
-  ## Eligibility
-  base.elig <- which(active == 1 & status == 0 & is.na(prepStartTime))
-  if (prep.elig.model == "base") {
-    idsElig <- base.elig
-  }
-  if (prep.elig.model == "ai.mono2.nt.3mo") {
-    mat <- dat$riskh$ai.mono2.nt.3mo
-    idsElig <- intersect(which(rowSums(mat) > 0), base.elig)
-  }
-  if (prep.elig.model == "ai.mono2.nt.6mo") {
-    mat <- dat$riskh$ai.mono2.nt.6mo
-    idsElig <- intersect(which(rowSums(mat) > 0), base.elig)
-  }
-  if (prep.elig.model == "ai.mono1.nt.3mo") {
-    mat <- dat$riskh$ai.mono1.nt.3mo
-    idsElig <- intersect(which(rowSums(mat) > 0), base.elig)
-  }
-  if (prep.elig.model == "ai.mono1.nt.6mo") {
-    mat <- dat$riskh$ai.mono1.nt.6mo
-    idsElig <- intersect(which(rowSums(mat) > 0), base.elig)
-  }
-  if (prep.elig.model == "uai.nonmonog") {
-    mat <- dat$riskh$uai.nonmonog
-    idsElig <- intersect(which(rowSums(mat) > 0), base.elig)
-  }
-  if (prep.elig.model == "uai.nmain") {
-    mat <- dat$riskh$uai.nmain
-    idsElig <- intersect(which(rowSums(mat) > 0), base.elig)
-  }
-  if (prep.elig.model == "ai.sd.mc") {
-    mat <- dat$riskh$ai.sd.mc
-    idsElig <- intersect(which(rowSums(mat) > 0), base.elig)
-  }
-  if (prep.elig.model == "uai.sd.mc") {
-    mat <- dat$riskh$uai.sd.mc
-    idsElig <- intersect(which(rowSums(mat) > 0), base.elig)
+  # Base eligibility
+  idsEligStart <- which(active == 1 & status == 0 & prepStat == 0 & lnt == at)
+
+  idsEligStop <- NULL
+  if (prep.risk.reassess == TRUE) {
+    idsEligStop <- which(active == 1 & prepStat == 1 & lnt == at)
   }
 
+  # Core eligiblity scenarios
+  if (prep.elig.model != "base") {
+    mat <- dat$riskh[[prep.elig.model]]
+    idsEligStart <- intersect(which(rowSums(mat) > 0), idsEligStart)
+    idsEligStop <- intersect(which(rowSums(mat) == 0), idsEligStop)
+  }
 
-  prepElig[idsElig] <- 1
-  prepEligTime[idsElig] <- at
+  prepElig[idsEligStart] <- 1
+  prepElig[idsEligStop] <- 0
 
 
-  ## Initiation
+  ## Stoppage ------------------------------------------------------------------
 
-  # Process
+  # Diagnosis
+  idsStpDx <- which(active == 1 & prepStat == 1 & diag.status == 1)
+
+  # Death
+  idsStpDth <- which(active == 0 & prepStat == 1)
+
+  # Transition to ineligibility
+  idsStpInelig <- idsEligStop
+
+  # Reset PrEP status
+  idsStp <- c(idsStpDx, idsStpDth, idsStpInelig)
+  prepStat[idsStp] <- 0
+
+
+  ## Initiation ----------------------------------------------------------------
+
   if (prep.cov.method == "curr") {
     prepCov <- sum(prepStat == 1, na.rm = TRUE)/sum(prepElig == 1, na.rm = TRUE)
   }
@@ -90,7 +86,7 @@ prep.mard <- function(dat, at) {
   nStart <- max(0, min(nEligSt, round((prep.coverage - prepCov) *
                                              sum(prepElig == 1, na.rm = TRUE))))
   idsStart <- NULL
-  if (nStart > 0 & at >= prep.start) {
+  if (nStart > 0) {
     if (prep.cov.rate >= 1) {
       idsStart <- ssample(idsEligSt, nStart)
     } else {
@@ -101,39 +97,23 @@ prep.mard <- function(dat, at) {
   # Attributes
   if (length(idsStart) > 0) {
     prepStat[idsStart] <- 1
-    prepStartTime[idsStart] <- at
     prepEver[idsStart] <- 1
     prepClass[idsStart] <- sample(c("l", "m", "h"), length(idsStart),
                                   TRUE, prep.class.prob)
   }
 
 
-  ## Stoppage
-
-  # Stop after diagnosis
-  idsStpDx <- which(active == 1 & prepStat == 1 & diag.status == 1)
-
-  # Stop after death
-  idsStpDth <- which(active == 0 & prepStat == 1)
+  ## Output --------------------------------------------------------------------
 
   # Attributes
-  idsStp <- c(idsStpDx, idsStpDth)
-  if (length(idsStp) > 0) {
-    prepStat[idsStp] <- 0
-  }
-
-
-  ## Output
   dat$attr$prepElig <- prepElig
-  dat$attr$prepEligTime <- prepEligTime
   dat$attr$prepStat <- prepStat
-  dat$attr$prepStartTime <- prepStartTime
   dat$attr$prepEver <- prepEver
   dat$attr$prepClass <- prepClass
 
   # Summary Statistics
   dat$epi$prepCov[at] <- prepCov
-  dat$epi$prepElig[at] <- length(idsElig)
+  dat$epi$prepElig[at] <- length(idsEligStart)
   dat$epi$prepStart[at] <- length(idsStart)
 
   return(dat)
