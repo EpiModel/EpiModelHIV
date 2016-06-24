d0fff0c2b677cf2559c59e014cfa8f1dd54274ac
smjenness <sjenness@uw.edu>
2016-06-24 11:53:40 -0400

    Add 2 non-HPC EpiModelHPC utilities

--- R/utilities.R
+++ R/utilities.R
@@ -106,6 +106,55 @@ reallocate_pcp <- function(in.pcp = c(0.211, 0.07, 0.1, 0.619), reall = 0) {
 }
 
 
+#' @title Truncate Simulation Time Series
+#'
+#' @description Left-truncates a simulation epidemiological summary statistics and
+#'              network statistics at a specified time step.
+#'
+#' @param x Object of class \code{netsim}.
+#' @param at Time step at which to left-truncate the time series.
+#'
+#' @details
+#' This function would be used when running a follow-up simulation from time steps
+#' \code{b} to \code{c} after a burnin period from time \code{a} to \code{b},
+#' where the final time window of interest for data analysis is \code{b} to \code{c}
+#' only.
+#'
+#' @export
+#'
+truncate_sim <- function(x, at) {
+
+  rows <- at:(x$control$nsteps)
+
+  # epi
+  x$epi <- lapply(x$epi, function(r) r[rows, ])
+
+  # control settings
+  x$control$start <- 1
+  x$control$nsteps <- max(seq_along(rows))
+
+  return(x)
+}
+
+
+
+#' @title Source All Files in a Directory
+#'
+#' @description Loops over all files in a directory to source them to the Global Environment.
+#'
+#' @param path Directory of files to source.
+#' @param trace Print names of sourced files to console.
+#' @param ... Additional arguments passed to \code{source}.
+#'
+#' @export
+#'
+sourceDir <- function(path, trace = TRUE, ...) {
+  for (nm in list.files(path, pattern = "\\.[RrSsQq]$")) {
+    if(trace) cat(nm,":")
+    source(file.path(path, nm), ...)
+    if(trace) cat("\n")
+  }
+}
 
 
 nbsdtosize <- function(mu, sd) {
