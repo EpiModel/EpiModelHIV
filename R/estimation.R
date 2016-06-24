6b89e4ed96d1ec799965f9c7e54b24570df9704b
smjenness <sjenness@uw.edu>
2016-06-24 09:29:49 -0400

    Move assign_degree to estimation.R

--- R/estimation.R
+++ R/estimation.R
@@ -425,3 +425,68 @@ base_nw_msm <- function(nwstats) {
 
   return(nw)
 }
+
+
+#' @title Assign Degree Vertex Attribute on Network Objects
+#'
+#' @description Assigns the degree vertex attributes on network objects
+#'              conditional on their values from the other networks.
+#'
+#' @param nw Object of class \code{network} that is the target for the vertex
+#'        attribute.
+#' @param deg.type Type of degree to assign to \code{nw}, with options of
+#'        \code{"pers"} to assign casual degree onto main network and
+#'        \code{"main"} to assign main degree to casual network.
+#' @param nwstats Object of class \code{nwstats}.
+#'
+#' @details
+#' This function assigns the degree of other networks as a vertex attribute on the
+#' target network given a bivariate degree mixing matrix of main, casual, and
+#' one-partnerships contained in the \code{nwstats} data.
+#'
+#' @seealso \code{\link{update_degree}}
+#' @export
+#'
+assign_degree <- function(nw, deg.type, nwstats) {
+  
+  if (!("network" %in% class(nw))) {
+    stop("nw must be of class network")
+  }
+  
+  if (deg.type == "main") {
+    attr.name <- "deg.main"
+    dist.B <- rowSums(nwstats$deg.mp.B)
+    dist.W <- rowSums(nwstats$deg.mp.W)
+  }
+  if (deg.type == "pers") {
+    attr.name <- "deg.pers"
+    dist.B <- colSums(nwstats$deg.mp.B)
+    dist.W <- colSums(nwstats$deg.mp.W)
+  }
+  
+  if (sum(dist.B) != 1 || sum(dist.W) != 1) {
+    stop("One of the degree distributions do not sum to 1")
+  }
+  
+  race <- get.vertex.attribute(nw, "race")
+  vB <- which(race == "B")
+  vW <- which(race == "W")
+  nB <- length(vB)
+  nW <- length(vW)
+  
+  num.degrees.B <- length(dist.B)
+  num.degrees.W <- length(dist.W)
+  
+  deg.B <- apportion_lr(nB, 0:(num.degrees.B - 1), dist.B, shuffled = TRUE)
+  deg.W <- apportion_lr(nW, 0:(num.degrees.W - 1), dist.W, shuffled = TRUE)
+  
+  if (nwstats$method == 2) {
+    deg.B <- paste0("B", deg.B)
+    deg.W <- paste0("W", deg.W)
+  }
+  
+  nw <- set.vertex.attribute(nw, attrname = attr.name, value = deg.B, v = vB)
+  nw <- set.vertex.attribute(nw, attrname = attr.name, value = deg.W, v = vW)
+  
+  return(nw)
+}
