19ac1997514320a75573043cec537bb00e2664be
Samuel Jenness <sjenness@uw.edu>
2016-06-28 17:12:51 -0400

    Updates to sourceDir

--- R/utilities.R
+++ R/utilities.R
@@ -140,19 +140,30 @@ truncate_sim <- function(x, at) {
 
 #' @title Source All Files in a Directory
 #'
-#' @description Loops over all files in a directory to source them to the Global Environment.
+#' @description Loops over all files in a directory to source them to the
+#'              Global Environment.
 #'
 #' @param path Directory of files to source.
-#' @param trace Print names of sourced files to console.
+#' @param verbose Print names of sourced files to console.
 #' @param ... Additional arguments passed to \code{source}.
 #'
 #' @export
 #'
-sourceDir <- function(path, trace = TRUE, ...) {
-  for (nm in list.files(path, pattern = "\\.[RrSsQq]$")) {
-    if(trace) cat(nm,":")
+sourceDir <- function(path, verbose = TRUE, ...) {
+
+  fn <- list.files(path, pattern = "\\.[Rr]$")
+  if (length(fn) == 0) {
+    stop("No R files in that path", call. = FALSE)
+  }
+
+  if (verbose == TRUE) {
+    cat("\n Sourced Files in", path,
+        "\n -----------------")
+  }
+
+  for (nm in fn) {
+    if (verbose == TRUE) cat("\n", nm)
     source(file.path(path, nm), ...)
-    if(trace) cat("\n")
   }
 }
 
