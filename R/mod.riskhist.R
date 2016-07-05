dda3482996e2f064087402b01669ec0efd89aca4
Samuel Jenness <sjenness@uw.edu>
2016-07-05 00:06:45 -0400

    Use dplyr group_by methods for riskhist module
    This is 20x faster by R::by

--- R/mod.riskhist.R
+++ R/mod.riskhist.R
@@ -23,8 +23,9 @@ riskhist_msm <- function(dat, at) {
   pri <- ceiling(dat$param$prep.risk.int)
 
   ## Edgelist, adds uai summation per partnership from act list
-  al <- dat$temp$al
-  uai <- as.numeric(by(al[, "uai"], al[, "pid"], sum))
+  al <- as.data.frame(dat$temp$al)
+  by_pid <- group_by(al, pid)
+  uai <- summarise(by_pid, uai = sum(uai))[, 2]
   el <- as.data.frame(cbind(dat$temp$el, uai))
 
   # Remove concordant positive edges
