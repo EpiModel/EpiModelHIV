c09ee38a6566830629a43773a6812bb1317cc9b5
kweiss2 <kevin.weiss@emory.edu>
2016-10-13 14:48:48 -0400

    Add syph progress

--- inst/netsim.mods.R
+++ inst/netsim.mods.R
@@ -16,22 +16,10 @@ control <- control_msm(simno = 0.253,
                        nsteps = 52,
                        nsims = 5, 
                        ncores = 1,
-                       initialize.FUN = initialize_msm, aging.FUN = aging_msm,
-                       deaths.FUN = deaths_msm, births.FUN = births_msm, test.FUN = test_msm,
-                       tx.FUN = tx_msm, prep.FUN = prep_msm, progress.FUN = progress_msm,
-                       progresssyph.FUN = progress_syph_msm,
-                       vl.FUN = vl_msm, aiclass.FUN = NULL, roleclass.FUN = NULL,
-                       resim_nets.FUN = simnet_msm, disclose.FUN = disclose_msm,
-                       acts.FUN = acts_msm, condoms.FUN = condoms_msm,
-                       riskhist.FUN = riskhist_msm, position.FUN = position_msm,
-                       trans.FUN = trans_msm, stitrans.FUN = sti_trans,
-                       stirecov.FUN = sti_recov, stitx.FUN = sti_tx, prev.FUN = prevalence_msm,
-                       verbose.FUN = verbose_msm,
                        save.nwstats = TRUE,
                        verbose.int = 1)
 
 sim <- netsim(est, param, init, control)
-
 
 
 at <- 1
