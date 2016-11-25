a1989d6a7cf947d881a68cd68c03db1cd418146f
Samuel Jenness <sjenness@uw.edu>
2016-11-25 09:58:17 -0500

    Start branch to test FastEl implementation

--- inst/netsim.mods.R
+++ inst/netsim.mods.R
@@ -1,21 +1,20 @@
-rm(list=ls())
+rm(list = ls())
 suppressMessages(library("EpiModelHIV"))
-sourceDir("R/")
 
 data(est)
 data(st)
 est
 st
-param <- param_msm(nwstats = st, 
+param <- param_msm(nwstats = st,
                    ai.scale = 1.323,
                    prep.coverage = 0)
-init <- init_msm(nwstats = st, 
-                 prev.B = 0.253, 
+init <- init_msm(nwstats = st,
+                 prev.B = 0.253,
                  prev.W = 0.253)
-control <- control_msm(simno = 0.253, 
+control <- control_msm(simno = 0.253,
                        nsteps = 52,
-                       nsims = 5, 
-                       ncores = 1, 
+                       nsims = 5,
+                       ncores = 1,
                        save.nwstats = TRUE,
                        verbose.int = 1)
 sim <- netsim(est, param, init, control)
@@ -34,11 +33,9 @@ dat <- test_msm(dat, at)
 dat <- tx_msm(dat, at)
 dat <- prep_msm(dat, at)
 dat <- progress_msm(dat, at)
-dat <- update_vl_msm(dat, at)
-dat <- update_aiclass_msm(dat, at)
-dat <- update_roleclass_msm(dat, at)
-dat <- edges_correct_msm(dat, at)
-dat <- updatenwp_msm(dat, at)
+dat <- vl_msm(dat, at)
+# dat <- update_aiclass_msm(dat, at)
+# dat <- update_roleclass_msm(dat, at)
 dat <- simnet_msm(dat, at)
 dat <- disclose_msm(dat, at)
 dat <- acts_msm(dat, at)
@@ -47,5 +44,3 @@ dat <- riskhist_msm(dat, at)
 dat <- position_msm(dat, at)
 dat <- trans_msm(dat, at)
 dat <- prevalence_msm(dat, at)
-verbose_msm(dat, type = "progress", s = 1, at)
-
