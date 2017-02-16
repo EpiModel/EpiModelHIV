676295aff20aed8292413e05870bbf24fc491a05
kweiss2 <kevin.weiss@emory.edu>
2017-02-16 15:23:00 -0500

    Test script values

--- inst/netsim.mods.R
+++ inst/netsim.mods.R
@@ -10,35 +10,35 @@ data(st)
 est
 st
 param <- param_msm(nwstats = st,
-                   ai.scale = 1.12, # 1.11889726, # was 1.13
+                   ai.scale = 1.13, # 1.11889726, # was 1.13
                    
-                   rsyph.tprob = 0.045, # 0.050,
-                   usyph.tprob = 0.035, #0.038, 
+                   rsyph.tprob = 0.048, # 0.050,
+                   usyph.tprob = 0.038, #0.038, 
                    
-                   hiv.rsyph.rr = 2.50, 
+                   hiv.rsyph.rr = 2.70, 
                    hiv.usyph.rr = 1.70,
-                   syph.hiv.rr = 1.70,
+                   syph.hiv.rr = 1.90,
                    
-                   rgc.tprob = 0.40, 
+                   rgc.tprob = 0.41, 
                    ugc.tprob = 0.31, 
-                   rct.tprob = 0.19, 
-                   uct.tprob = 0.145,
+                   rct.tprob = 0.21, 
+                   uct.tprob = 0.15,
                    
-                   hiv.rgc.rr = 2.50, #2.780673,
+                   hiv.rgc.rr = 2.70, #2.780673,
                    hiv.ugc.rr = 1.70, #1.732363,
-                   hiv.rct.rr = 2.50, #2.780673,
+                   hiv.rct.rr = 2.70, #2.780673,
                    hiv.uct.rr = 1.70, #1.732363,
                    
                    # adjust prim and seco from 0.1385 each
                    stage.syph.B.prob = c(0.00, 0.20, 0.077, 0.277, 0.22, 0.22, 0.006),
                    stage.syph.W.prob = c(0.00, 0.20, 0.077, 0.277, 0.22, 0.22, 0.006),
                    
-                   syph.prim.sympt.prob.tx = 0.45, # Tuite PLoS One 2014, Bissessor AIDS 2010, Kourbatova STD 2008 use 0.45
+                   syph.prim.sympt.prob.tx = 0.48201266, # Tuite PLoS One 2014, Bissessor AIDS 2010, Kourbatova STD 2008 use 0.45
                    syph.prim.asympt.prob.tx = 0.00,
-                   syph.seco.sympt.prob.tx = 0.70, # Tuite PLoS One 2014, Bissessor AIDS 2010, Kourbatova STD 2008
+                   syph.seco.sympt.prob.tx = 0.67214403, # Tuite PLoS One 2014, Bissessor AIDS 2010, Kourbatova STD 2008
                    syph.seco.asympt.prob.tx = 0.00,
-                   syph.earlat.prob.tx = 0.10, # Tuite PLoS One 2014, Bissessor AIDS 2010, Kourbatova STD 2008
-                   syph.latelat.prob.tx = 0.20,
+                   syph.earlat.prob.tx = 0.17136638, # Tuite PLoS One 2014, Bissessor AIDS 2010, Kourbatova STD 2008
+                   syph.latelat.prob.tx = 0.15437417,
                    syph.tert.sympt.prob.tx = 0.90,
                    syph.tert.asympt.prob.tx = 0.00,
                    
