e94e922c13483e9f779a77258ae208b3faeb41e7
Sarah Stansfield <sestansfield@gmail.com>
2017-04-17 16:45:27 -0700

    Update test-netsim.R

--- tests/testthat/test-netsim.R
+++ tests/testthat/test-netsim.R
@@ -5,9 +5,9 @@ test_that("Burnin model", {
   data(st)
   data(est)
 
-  param <- param_msm(nwstats = st)
-  init <- init_msm(nwstats = st)
-  control <- control_msm(simno = 1, nsteps = 10, verbose = FALSE)
+  param <- param.mard(nwstats = st)
+  init <- init.mard(nwstats = st)
+  control <- control.mard(simno = 1, nsteps = 10, verbose = FALSE)
 
   sim <- netsim(est, param, init, control)
   expect_is(sim, "netsim")
