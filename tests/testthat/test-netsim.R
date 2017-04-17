7471fa7a20aece7252901e668318a71cab7a9b6a
Sarah Stansfield <sestansfield@gmail.com>
2017-04-17 16:50:29 -0700

    Update test-netsim.R

--- tests/testthat/test-netsim.R
+++ tests/testthat/test-netsim.R
@@ -19,24 +19,24 @@ test_that("Follow-up model", {
   data(st)
   data(est)
 
-  param <- param_msm(nwstats = st,
+  param <- param.mard(nwstats = st,
                      prep.start = 10)
-  init <- init_msm(nwstats = st)
-  control <- control_msm(simno = 1, nsteps = 10,
+  init <- init.mard(nwstats = st)
+  control <- control.mard(simno = 1, nsteps = 10,
                          save.other = c("attr", "temp", "riskh", "el", "p"),
                          verbose = FALSE)
 
   sim <- netsim(est, param, init, control)
 
-  param <- param_msm(nwstats = st,
+  param <- param.mard(nwstats = st,
                      prep.start = 10,
                      prep.elig.model = "cdc3",
                      prep.coverage = 0.5,
                      prep.risk.int = 182,
                      prep.class.prob = reallocate_pcp(reall = 0),
                      prep.class.hr = c(1, 0.69, 0.19, 0.05))
-  init <- init_msm(nwstats = st)
-  control <- control_msm(simno = 1, start = 11, nsteps = 20,
+  init <- init.mard(nwstats = st)
+  control <- control.mard(simno = 1, start = 11, nsteps = 20,
                          verbose = FALSE, initialize.FUN = reinit_msm)
 
   sim2 <- netsim(sim, param, init, control)
