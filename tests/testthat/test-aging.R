953c7610419dfb181a1e5946cf893fc51231442f
smjenness <sjenness@uw.edu>
2017-04-18 10:35:02 -0400

    Add unit test for aging module

--- tests/testthat/test-aging.R
+++ tests/testthat/test-aging.R
@@ -1,6 +1,23 @@
 context("Aging Module")
 
-test_that("Aging module functions properly", {
+data(est)
+data(st)
 
-  NULL
+test_that("Aging module", {
+
+  param <- param_msm(nwstats = st)
+  init <- init_msm(nwstats = st)
+  control <- control_msm()
+
+  at <- 1
+  dat <- initialize_msm(est, param, init, control, s = 1)
+
+  pre.age <- dat$attr$age
+  expect_true(all(!is.na(pre.age)))
+  expect_true(sum(pre.age) > 0)
+
+  dat <- aging_msm(dat, at = 2)
+  post.age <- dat$attr$age
+  expect_true(all(post.age == pre.age + st$time.unit/365))
+
 })
