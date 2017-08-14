e6ae332a7b54738fb4760f53c76f6794c45854df
Kevin Weiss <kevin.weiss@emory.edu>
2017-08-14 16:29:15 -0400

    Netsim.mods.R update with new probabilities

--- inst/netsim.mods.R
+++ inst/netsim.mods.R
@@ -3,8 +3,10 @@ suppressMessages(library("EpiModelHIV"))
 suppressMessages(library("dplyr"))
 devtools::load_all()
 
-data(est)
-data(st)
+#data(est)
+#data(st)
+load("est/nwstats.rda")
+load("est/fit.rda")
 
 param <- param_msm(nwstats = st,
                    ai.scale = 1.03,
@@ -20,22 +22,18 @@ param <- param_msm(nwstats = st,
                    syph.tert.prog.prob = 0.00015625599,
 
                    # STI acquisition
-                   rgc.tprob = 0.447,
-                   ugc.tprob = 0.337,
-                   rct.tprob = 0.2025,
-                   uct.tprob = 0.1825,
-                   rsyph.tprob = 0.1526957,
-                   usyph.tprob = 0.1326838,
+                   rgc.tprob = 0.4434,
+                   ugc.tprob = 0.3343,
+                   rct.tprob = 0.2008,
+                   uct.tprob = 0.1790,
+                   syph.tprob = 0.1424,
 
                    # HIV acquisition
-                   URAI.prob = 0.0082 * 1.09,
-                   UIAI.prob = 0.0031 * 1.09,
                    hiv.rgc.rr = 1.80292790,
                    hiv.ugc.rr = 1.1989083,
                    hiv.rct.rr = 1.80292790,
                    hiv.uct.rr = 1.1989083,
-                   hiv.rsyph.rr = 1.80292790,
-                   hiv.usyph.rr = 1.1989083,
+                   hiv.syph.rr = 1.62,
 
                    # HIV transmission
                    hiv.trans.gc.rr = 1,
@@ -57,13 +55,13 @@ param <- param_msm(nwstats = st,
                    hivdx.syph.sympt.tx.rr = 1.45,
 
                    prep.coverage = 0.0,
-                   ept.coverage = 0.9,
+                   ept.coverage = 0.8,
                    stianntest.coverage = 0.1,
                    stihighrisktest.coverage = 0.1,
 
                    prep.start = 2600,
                    stitest.start = 2600,
-                   ept.start = 2,
+                   ept.start = 5,
 
                    stitest.elig.model = "sti",
 
@@ -76,14 +74,14 @@ init <- init_msm(nwstats = st,
                  prev.W = 0.08,
                  prev.ugc = 0.010,
                  prev.rgc = 0.010,
-                 prev.uct = 0.015,
-                 prev.rct = 0.015,
+                 prev.uct = 0.010,
+                 prev.rct = 0.010,
                  prev.syph.B = 0.010,
                  prev.syph.W = 0.010,
                  stage.syph.B.prob = c(0.00, 0.01, 0.01, 0.10, 0.44, 0.44, 0.00),
                  stage.syph.W.prob = c(0.00, 0.01, 0.01, 0.10, 0.44, 0.44, 0.00))
 
-control <- control_msm(nsteps = 2600)
+control <- control_msm(nsteps = 200)
 
 sim <- netsim(est, param, init, control)
 
@@ -91,7 +89,7 @@ at <- 1
 dat <- initialize_msm(est, param, init, control, s = 1)
 # dat <- reinit_msm(sim, param, init, control, s = 1)
 
-debug(simnet_msm)
+debug(sti_ept_msm)
 
 at <- at + 1
 for (at in 2:control$nsteps) {
