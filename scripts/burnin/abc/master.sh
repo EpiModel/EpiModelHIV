5190db45b3aeb56cfff90093ac23fa112ca4cced
dth2 <dth2@u.washington.edu>
2017-11-30 09:25:03 -0800

    Move scripts being used and delete scripts form ealier projects not being used.

--- scripts/burnin/abc/master.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-qsub -q batch -m ae -v NSIM=200,PACC=0.01 runsim.burn.abcsmc4.sh
-qsub -q batch -m ae -v NSIM=200,PACC=0.05 runsim.burn.abcsmc4.sh
-qsub -q batch -m ae -v NSIM=200,PACC=0.10 runsim.burn.abcsmc4.sh
-qsub -q batch -m ae -v NSIM=200,PACC=0.20 runsim.burn.abcsmc4.sh
