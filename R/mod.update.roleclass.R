7a82a8b250fdda7e74233048dbfee868d83e2ac7
dth2 <dth2@u.washington.edu>
2017-12-11 13:50:49 -0800

    only change role class if there are eligible nodes.

--- R/mod.update.roleclass.R
+++ R/mod.update.roleclass.R
@@ -16,6 +16,12 @@
 #'
 roleclass_trans <- function(dat, at) {
 
+
+  ids.B.r <-NULL
+  ids.B.v <-NULL
+  ids.W.r <-NULL 
+  ids.W.v <-NULL
+  
   role.class <- dat$attr$role.class
   role.shift <- dat$param$role.shift
   
@@ -23,6 +29,7 @@ roleclass_trans <- function(dat, at) {
   deg.pers <- get_degree(dat$el[[2]])
   deg.asmm <- get_degree(dat$el[[3]])
   
+
   ids.B.r <- which(dat$attr$race == "B" & dat$attr$role.class == "R" & dat$attr$age >= 19 & dat$attr$age <= 19.03 & deg.main == 0 & deg.pers == 0 & deg.asmm == 0)
   ids.B.v <- which(dat$attr$race == "B" & dat$attr$role.class == "V" & dat$attr$age >= 19 & dat$attr$age <= 19.03 & deg.main == 0 & deg.pers == 0 & deg.asmm == 0)
   
@@ -32,11 +39,11 @@ roleclass_trans <- function(dat, at) {
   ir.list<-c(rep("I",role.shift[1]*1000),rep("R",(1-role.shift[1])*1000))
   iv.list<-c(rep("I",role.shift[2]*1000),rep("V",(1-role.shift[2])*1000))
   
-  role.class[ids.B.r] <- sample(ir.list,size=length(ids.B.r)) 
-  role.class[ids.B.v] <- sample(iv.list,size=length(ids.B.v)) 
+  if (length(ids.B.r) > 1) {role.class[ids.B.r] <- sample(ir.list,size=length(ids.B.r)) }
+  if (length(ids.B.v) > 1) {role.class[ids.B.v] <- sample(iv.list,size=length(ids.B.v)) }
   
-  role.class[ids.W.r] <- sample(ir.list,size=length(ids.W.r))
-  role.class[ids.W.v] <- sample(iv.list,size=length(ids.B.v)) 
+  if (length(ids.W.r) > 1) {role.class[ids.W.r] <- sample(ir.list,size=length(ids.W.r)) }
+  if (length(ids.W.v) > 1) {role.class[ids.W.v] <- sample(iv.list,size=length(ids.B.v)) }
 
   
   dat$attr$role.class <- role.class
