ef260936d1a0ca261b0cb5a5c48e890ec0d5f855
dth2 <dth2@u.washington.edu>
2018-01-29 08:55:05 -0800

    New function for followup creates dataframes, matices and lists to track new elements of dat that start after the burnin

---
+++ R/mod.FU.elements.R
@@ -0,0 +1,49 @@
+
+#' @title Follow-up elements Module
+#'
+#' @description Module sets up adolcent risk history and infection age elements of dat.
+#'
+#' @inheritParams FU_elements_camplc
+#'
+#' @param dat Master data list object of class \code{dat} containing networks,
+#'        individual-level attributes, and summary statistics.
+#' @param at Current time step.
+#'
+#' @return
+#' This function returns \code{dat} after adding elements to dat that are not tracked during the burnin
+#' \code{riskhist} , \code{age.inf.vec}, \code{counts}. 
+#'
+#' @keywords module
+#' @export
+#'
+FU_elements_camplc <- function(dat, at) {
+ 
+  
+  if (dat$param$FU == TRUE & dat$control$start == at) {
+  # Risk history lists adol 
+  t1<-t2<-t3<-t4<-t5<-t6<-t7<-t8<-t9<-t10<-t11<-t12<-t13<-t14<-t15<-t16<-t17<-t18<-t19<-t20<-t21<-t22<-t23<-t24<-t25<-t26<-rep(0,length(dat$attr$uid))
+  dat$riskhist<-cbind(dat$attr$uid,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15,t16,t17,t18,t19,t20,t21,t22,t23,t24,t25,t26)
+  dat$riskhist<-as.data.frame(dat$riskhist)
+  
+  
+  #data frames for tracking race age and risk catagory specific UAI counts and PrEP status
+  dat$counts$uaiCounts.B <-matrix(nrow=6, ncol=5)
+  dat$counts$uaiCounts.W <-matrix(nrow=6, ncol=5)
+  dat$counts$prepCounts.B <-matrix(nrow=6, ncol=5)
+  dat$counts$prepCounts.W <-matrix(nrow=6, ncol=5)
+  dat$counts$status.B <-matrix(nrow=6, ncol=5)
+  dat$counts$status.W <-matrix(nrow=6, ncol=5)
+  dat$counts$tx.B <-matrix(nrow=6, ncol=5)
+  dat$counts$tx.W <-matrix(nrow=6, ncol=5)
+  dat$counts$fsupp.B <-matrix(nrow=6, ncol=5)
+  dat$counts$fsupp.W <-matrix(nrow=6, ncol=5)
+  dat$counts$dx.B <-matrix(nrow=6, ncol=5)
+  dat$counts$dx.W <-matrix(nrow=6, ncol=5)
+   
+  #Track the age at which each new infection is aquired
+  dat$age.inf.vec <- list()
+  
+  }
+
+  return(dat)
+}
