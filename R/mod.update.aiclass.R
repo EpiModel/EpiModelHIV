df1b0c96d2c0434a9cef4c0579ece0341075e70d
dth2 <dth2@u.washington.edu>
2018-04-25 17:52:54 -0700

    change inherits now that _msm is outside the package

--- R/mod.update.aiclass.R
+++ R/mod.update.aiclass.R
@@ -4,7 +4,7 @@
 #' @description Module function for updating act class in one-off partnerships
 #'              based on probabilities of transition.
 #'
-#' @inheritParams aging_msm
+#' @inheritParams aging_shamp
 #'
 #' @return
 #' This function updates the individual-level attribute \code{inst.ai.class} on
