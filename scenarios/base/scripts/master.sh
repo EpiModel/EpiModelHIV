9eb6a69f429289d94ac64df5794e6b889a1190a1
dth2 <dth2@u.washington.edu>
2019-01-14 10:09:49 -0800

    new array test

--- scenarios/base/scripts/master.sh
+++ scenarios/base/scripts/master.sh
@@ -1,9 +1,11 @@
+#!/bin/bash 
 
 # Run batch queue
 #sbatch -p csde -A csde runsim.sh 
 
 #Multiple Jobs
-#sbatch -p csde -A csde --export=SIMNO=1000,NJOBS=4 runsim.sh 
+#sbatch -p csde -A csde --export=ALL,SIMNO=50,NJOBS=5 runsim.sh 
 
 # Array job
-sbatch -p csde -A csde --array=1-6 --export=ALL,SIMNO=1000,NJOBS=6 runsim.sh 
+sbatch -p csde -A csde --array=1-5 --export=ALL,SIMNO=80,NJOBS=5 runsim.sh 
+
