18a2697082449d74799c9ee51ad17db9df3b8540
dth2 <dth2@u.washington.edu>
2019-03-07 16:29:58 -0800

    drop array

--- scenarios/base/scripts/master.sh
+++ scenarios/base/scripts/master.sh
@@ -4,8 +4,8 @@
 #sbatch -p csde -A csde runsim.sh 
 
 #Multiple Jobs
-#sbatch -p csde -A csde --export=ALL,SIMNO=50,NJOBS=5 runsim.sh 
+sbatch -p csde -A csde --export=ALL,SIMNO=50,NJOBS=1 runsim.sh 
 
 # Array job
-sbatch -p csde -A csde --array=1-5 --export=ALL,SIMNO=80,NJOBS=5 runsim.sh 
+#sbatch -p csde -A csde --array=1-5 --export=ALL,SIMNO=80,NJOBS=5 runsim.sh 
 
