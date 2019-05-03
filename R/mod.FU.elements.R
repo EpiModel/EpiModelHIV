52460bf3205411d31b01763e26d7607f49160735
dth2 <dth2@u.washington.edu>
2019-05-03 13:14:42 -0700

    only track UAI in the last 6 months for ASMM if prep.risk.kist.asmm==TRUE.  The dataframe is very slow.  Delete the counts matrices.  They are not used.

--- R/mod.FU.elements.R
+++ R/mod.FU.elements.R
@@ -21,24 +21,15 @@ FU_elements_camplc <- function(dat, at) {
   
   if (dat$param$FU == TRUE & dat$control$start == at) {
   # Risk history lists adol 
+    
+  if (dat$param$prep.risk.hist.asmm == TRUE){
+    
   t1<-t2<-t3<-t4<-t5<-t6<-t7<-t8<-t9<-t10<-t11<-t12<-t13<-t14<-t15<-t16<-t17<-t18<-t19<-t20<-t21<-t22<-t23<-t24<-t25<-t26<-rep(0,length(dat$attr$uid))
   dat$riskhist<-cbind(dat$attr$uid,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15,t16,t17,t18,t19,t20,t21,t22,t23,t24,t25,t26)
   dat$riskhist<-as.data.frame(dat$riskhist)
   
-  
-  #data frames for tracking race age and risk catagory specific UAI counts and PrEP status
-  dat$counts$uaiCounts.B <-matrix(nrow=6, ncol=5)
-  dat$counts$uaiCounts.W <-matrix(nrow=6, ncol=5)
-  dat$counts$prepCounts.B <-matrix(nrow=6, ncol=5)
-  dat$counts$prepCounts.W <-matrix(nrow=6, ncol=5)
-  dat$counts$status.B <-matrix(nrow=6, ncol=5)
-  dat$counts$status.W <-matrix(nrow=6, ncol=5)
-  dat$counts$tx.B <-matrix(nrow=6, ncol=5)
-  dat$counts$tx.W <-matrix(nrow=6, ncol=5)
-  dat$counts$fsupp.B <-matrix(nrow=6, ncol=5)
-  dat$counts$fsupp.W <-matrix(nrow=6, ncol=5)
-  dat$counts$dx.B <-matrix(nrow=6, ncol=5)
-  dat$counts$dx.W <-matrix(nrow=6, ncol=5)
+  }
+    
    
   #Track the age at which each new infection is aquired
   dat$age.inf.vec <- list()
