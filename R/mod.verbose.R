aff6951f5de7f213f0b6b96a527bf2c8d8b16da9
dth2 <dth2@u.washington.edu>
2020-03-19 14:08:55 -0700

    coment out STI prev calcualtions

--- R/mod.verbose.R
+++ R/mod.verbose.R
@@ -54,10 +54,10 @@ verbose_msm <- function(x, type, s, at) {
           nsteps <- x$control$nsteps
           time.unit <- x$param$time.unit
           prev <- round(x$epi$i.prev[at], 3)
-          prev.rgc <- round(x$epi$prev.rgc[at], 3)
-          prev.ugc <- round(x$epi$prev.ugc[at], 3)
-          prev.rct <- round(x$epi$prev.rct[at], 3)
-          prev.uct <- round(x$epi$prev.uct[at], 3)
+          #prev.rgc <- round(x$epi$prev.rgc[at], 3)
+          #prev.ugc <- round(x$epi$prev.ugc[at], 3)
+          #prev.rct <- round(x$epi$prev.rct[at], 3)
+          #prev.uct <- round(x$epi$prev.uct[at], 3)
 
           cat("\014")
           cat("\nEpidemic Simulation")
@@ -73,10 +73,10 @@ verbose_msm <- function(x, type, s, at) {
           cat("\nHIV Cuml Incidence:", sum(x$epi$incid, na.rm = TRUE))
           cat("\nHIV Prevalence: ", x$epi$i.num[at], " (", prev, ")", sep = "")
           cat("\n------------------------------")
-          cat("\nrGC Prevalence: ", prev.rgc, sep = "")
-          cat("\nuGC Prevalence: ", prev.ugc, sep = "")
-          cat("\nrCT Prevalence: ", prev.rct, sep = "")
-          cat("\nuCT Prevalence: ", prev.uct, sep = "")
+          #cat("\nrGC Prevalence: ", prev.rgc, sep = "")
+          #cat("\nuGC Prevalence: ", prev.ugc, sep = "")
+          #cat("\nrCT Prevalence: ", prev.rct, sep = "")
+          #cat("\nuCT Prevalence: ", prev.uct, sep = "")
           cat("\n==============================")
 
         }
