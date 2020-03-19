7c03ca909fcffa8fe01f072bb127b585cd468a32
dth2 <dth2@u.washington.edu>
2020-03-19 09:19:41 -0700

    add a start time for the home testing intervention

--- R/params_seattle.R
+++ R/params_seattle.R
@@ -371,6 +371,7 @@ param_seattle_msm <- function(nwstats,
                       Risk.replace = FALSE,
                       Risk.supp = FALSE,
                       Never.test.supp = FALSE,
+                      HT.int.start = Inf,
                       
                      hometest.rep.opp.frac = .25,
                      hometest.rep.reg.frac = .25,
